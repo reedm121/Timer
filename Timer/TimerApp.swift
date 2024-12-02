3a5398210b18084f25d515b8170cf1267bf8f242
Reed Gantz <reedm121@gmail.com>
2024-12-02 14:05:01 -0500

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Reed Gantz on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
